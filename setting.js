0807fbd74a6c362e536bd13c404adf73f2246ea3
Novabot <novabot503@gmail.com>
2026-02-21 13:47:13 +0700

    Add files via upload

--- setting.js
+++ setting.js
@@ -7,7 +7,7 @@ module.exports = {
     VERSI_WEB: '1.0',
     URL: 'https://novacat-two.vercel.app',
     GITHUB_TOKEN: "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",
-    GITHUB_REPO: "novabot503/nova",
+    GITHUB_REPO: "novabot503/novacat",
     GITHUB_BRANCH: "main",
     GITHUB_PATH: "uploads"
 };
