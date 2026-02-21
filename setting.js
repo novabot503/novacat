0ff062104432f8ac19b397e54cf3506b9efa1433
Novabot <novabot503@gmail.com>
2026-02-21 13:41:39 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,13 @@
+module.exports = {
+    TELEGRAM_TOKEN: "8332999387:AAEPzYevi0TdcFX7K5GgSlqaPmnrltlVxj0",
+    OWNER_ID: "7587303225",
+    PORT: 8080,
+    HOST: 'localhost',
+    DEVELOPER: '@Novabot403',
+    VERSI_WEB: '1.0',
+    URL: 'https://novacat-two.vercel.app',
+    GITHUB_TOKEN: "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",
+    GITHUB_REPO: "novabot503/nova",
+    GITHUB_BRANCH: "main",
+    GITHUB_PATH: "uploads"
+};
